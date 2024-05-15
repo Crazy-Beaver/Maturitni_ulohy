fae47c54ee9c04b880dd2a4a851c021d1ea35d2f
Crazy Beaver <150656137+Crazy-Beaver@users.noreply.github.com>
2024-05-15 09:53:12 +0200

    pocet_cisel update, maximum new

--- maximum.py
+++ maximum.py
@@ -1,16 +1,12 @@
 import random
 
 seznam = []
-cisla = {}
 length = int(input())
 for i in range(length):
     seznam.append(random.randint(0, 100))
-
-for i in seznam:
-    if i in cisla:
-        cisla[i] += 1
-    else:
-        cisla[i] = 1
-
 print(seznam)
-print(cisla)
+max = seznam[0]
+for i in seznam:
+    if i>max:
+        max = i
+print(max)
