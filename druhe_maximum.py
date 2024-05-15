28b6b430b2f444db3ffcdb7fc036f945fd32ae9f
Crazy Beaver <150656137+Crazy-Beaver@users.noreply.github.com>
2024-05-15 10:26:37 +0200

    druhe_maximum update 2

--- druhe_maximum.py
+++ druhe_maximum.py
@@ -1,6 +1,6 @@
 import random
 
-seznam = []
+seznam = [99,99,98]
 length = int(input())
 for i in range(length):
     seznam.append(random.randint(0, 100))
@@ -16,4 +16,8 @@ for i in seznam:
         max = i
     elif i>max2 and i!=max:
         max2 = i
+    elif max == max2 and max2 != i:
+        max2 = i
+if max == max2:
+    max2 = "none"
 print(max, max2)
